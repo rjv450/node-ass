2486c18a69ffbd43b08c6e0bb0edfee4b7387ce1
rijovarghese450 <rijovarghese450@gmail.com>
2024-05-24 06:22:54 +0000

    add cors and helmet

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-A simple hello world express server. 
+
+## Environment Variables
+
+The following environment variables are required to run the application:
+
+- `PORT`: The port the application will listen on.
+- `DB_USER`: The username for the database.
+- `DB_PASSWORD`: The password for the database.
+- `DB_HOST`: The host of the database.
+- `DB_PORT`: The port of the database.
+- `COMPANY_DB`: The name of the database.
+- `JWT_SECRET_KEY`: The secret key used to sign JWTs.
+
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import authRoutes from "./src/routes/companyRouter.js";
 import productRoutes from "./src/routes/prodctRouter.js";
+import helmet from "helmet";
+import cors from "cors";
 const app = express();
 
 const port = parseInt(process.env.PORT) || 8080;
@@ -8,6 +10,11 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/product", productRoutes);
 app.use("/uploads", express.static("uploads"));
+
+app.use(helmet());
+app.use(cors());
+
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
